50c85f35e00dfaf049524aa9f5ca9620dadcec2b
Gabrieltx18 <gabrielteixeiraferreira18@gmail.com>
2025-11-10 02:09:03 -0300

    Login Updates and create menu

--- AppFit.py
+++ AppFit.py
@@ -6,14 +6,14 @@ import mysql.connector
 cnn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="root",
-    database="teste"
+    password="1234",
+    database="AppFit"
 )
 
 usuario=str(input('Digite seu Nome:'))
 senha=str(input('Digite sua Senha:'))
 
-query = "Select * from usuario where nm_usuario = %s and senha = %s"
+query = "Select * from user where nome = %s and senha = %s"
 cursor = cnn.cursor()
 cursor.execute(query, (usuario,senha))
     
@@ -23,108 +23,102 @@ if usuario_encontrado:
     
     print("Seja bem vindo",usuario)
       
-elif not usuario_encontrado :
-    
-    login = input("Deseja criar uma nova conta ? (Sim / Não) ").strip().lower()
+else:
+
+    print("Usuario ou senha incorretos")
+    login = input("Deseja criar uma nova conta ? (Sim / Nao) ").strip().lower()
     
     try:
         login == "Sim"
-        cmd = "INSERT INTO usuario (nm_usuario, senha) VALUES (%s, %s)"
-        cursor.execute(cmd, (usuario,senha))
+
+        altura= float(input('Digite sua altura em Centímetros:'))
+        peso = float(input('Digite seu peso em Kg:'))
+        idade = int(input('Digite sua idade:'))
+        sexo = str(input('Digite seu sexo (M - Masculino | F - Feminino): ')).strip().upper()
+
+        cmd = "INSERT INTO user (nome, senha, altura, peso, idade, sexo) VALUES (%s, %s, %s,%s, %s, %s)"
+        cursor.execute(cmd, (usuario,senha,altura,peso,idade,sexo))
         
         print("Conta criada com sucesso!")
         cnn.commit()
-    except:
-        print("Operação cancelada.")
-        
-else:
-    print("Usuario ou senha incorretos")
-    exit()
 
-
-altura=int(input('Digite sua altura em Centímetros:'))
-peso=float(input('Digite seu peso em Kg:'))
-idade=float(input('Digite sua idade:'))
-sexo = None     
+    except Exception as  e:
+        print(e)
+        exit()
 while True:
-    sexo = input('Digite seu sexo (M - Masculino | F - Feminino): ').strip().upper()
-    if sexo in ['M', 'MASCULINO']:
-        sexo = 'M'
-        break
-    elif sexo in ['F', 'FEMININO']:
-        sexo = 'F'
-        break
-    else:
-        print('Opção inválida! Digite M, Masculino, F ou Feminino.')
-# Definir fator de atividade física conforme quantidade de práticas
-exerc_p_semana = int(input('Quantas vezes por semana você pratica exercícios físicos? '))
-# Calcular taxa metabolica basal
-if sexo=='M':
-    tmb=66+(13.7*peso)+(5*altura)-(6.8*idade)
-    print(f'Sua taxa metabolica basal é de: {round(tmb)}')
-elif sexo=='F':
-    tmb=655+(9.6*peso)+(1.8*altura)-(4.7*idade)
-    print(f'Sua taxa metabolica basal é de: {round(tmb)}')
-#Media calculada de calorias gastas em um dia todo
-if exerc_p_semana <= 2:
-    f_ativ = 1.2
-elif exerc_p_semana <= 4:
-    f_ativ = 1.35
-elif exerc_p_semana <= 6:
-    f_ativ = 1.55
-elif exerc_p_semana >= 7:
-    f_ativ = 1.7
-media_diaria = tmb * f_ativ
-print(f'Seu gasto estimado calórica diária é de: {round(media_diaria)}kcal.')
-#calcular o imc da pessoa 
-imc = peso / ((altura / 100) ** 2)
-print(f'Seu IMC é de: {imc:.2f}')
-if imc < 18.5:
-    print('Você está abaixo do peso ideal.')
-elif 18.5 <= imc < 24.9:
-    print('Você está com o peso ideal.')
-elif 25 <= imc < 29.9:
-    print('Você está com sobrepeso.')
-elif 30 <= imc < 34.9:
-    print('Você está com Obesidade Grau I.')
-elif 35 <= imc < 39.9:
-    print('Você está com Obesidade Grau II.')
-elif imc >= 40:
-    print('Você está com Obesidade Grau III.')
-# Definir 5 exercicios diferentes
-print('Agora vamos calcular o gasto calórico do seu exercício físico.')
-print('Qual o exercício que você praticou?')
-print('0 - Nenhum')
-print('1 - Corrida (5:35 min/km)')
-print('2 - Caminhada')
-print('3 - Natação')
-print('4 - Ciclismo')
-print('5 - Musculação')
-exerc = input('Digite o número do exercício: ')
-if exerc == '0':
-    print('Nenhum exercício selecionado.')
-    input("Pressione Enter para sair...")
-    exit()
-tempo = int(input('Digite o tempo de exercício em minutos: '))
-# METs para os 5 exercícios principais
-met_dict = {
-    '1': 10.5, # Corrida (5:35 min/km)
-    '2': 4.0,  # Caminhada
-    '3': 8.0,  # Natação
-    '4': 7.0,  # Ciclismo
-    '5': 6.0   # Musculação
-}
-if exerc in met_dict:
-    met = met_dict[exerc]
-    gasto = met * peso * (tempo / 60)
-    print(f'O gasto calórico do seu exercício é de: {round(gasto)} kcal.')
-else:
-    print('Opção inválida. Encerrando o programa.')
-    exit()
-input("Pressione Enter para sair...")
 
-#Adiciona mais exercicios
-#Cria uma forma mais facil de transforma em .exe do programa
-   #python -m PyInstaller --onefile Projetin.py
-#Verificar como funcina o processo do if else de sexo e se possivel alteralo
-#Não usar algo muito complexo como "def"
+    print("\n=== MENU PRINCIPAL ===")
+    print("1 - Registrar Exercício")
+    print("2 - Calcular IMC/TMB/Gasto calórico diário")
+    print("3 - Meus últimos exercícios")
+    print("4 - Alterar perfil")
+    print("5 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+
+        print('1 - Corrida (5:35 min/km)')
+        print('2 - Caminhada')
+        print('3 - Natação')
+        print('4 - Ciclismo')
+        print('5 - Musculação')
+
+        exerc = input('Digite o número do exercício: ')
+
+        if exerc == '0':
+            print('Nenhum exercício selecionado.')
+
+    elif opcao == "2":
+
+        menu = input("Selecione uma das opções (IMC ou TMB): ")
+
+        if menu.upper() == "IMC":
+            imc = peso / ((altura / 100) ** 2)
+            print("Seu IMC é de:",round(imc, 2))
+
+            if imc < 18.5:
+                print('Você está abaixo do peso ideal.')
+            elif imc < 24.9:
+                print('Você está com o peso ideal.')
+            elif imc < 29.9:
+                print('Você está com sobrepeso.')
+            elif imc < 34.9:
+                print('Você está com Obesidade Grau I.')
+            elif imc < 39.9:
+                print('Você está com Obesidade Grau II.')
+            else:
+                print('Você está com Obesidade Grau III.')
+
+        elif menu.upper =="TMB":
+
+            exerc_p_semana = int(input('Quantas vezes por semana você pratica exercícios físicos? '))
+
+        if sexo == 'M':
+            tmb = 66 + (13.7 * peso) + (5 * altura) - (6.8 * idade)
+        elif sexo == 'F':
+            tmb = 655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade)
+        print(f'Sua taxa metabólica basal é de: {round(tmb)} kcal')
+
+        if exerc_p_semana <= 2:
+            f_ativ = 1.2
+        elif exerc_p_semana <= 4:
+            f_ativ = 1.35
+        elif exerc_p_semana <= 6:
+            f_ativ = 1.55
+        else:
+            f_ativ = 1.7
+
+        media_diaria = tmb * f_ativ
+        print(f'Seu gasto calórico diário estimado é de: {round(media_diaria)} kcal')
+
+
+
+    # OPÇÃO 5 - SAIR
+    elif opcao == "5":
+        print("Finalizando Programa... 👋")
+        break
+
+    # OUTRAS OPÇÕES
+    else:
+        print("Opção inválida! Tente novamente.")
